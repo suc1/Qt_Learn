44e5991a58cb522099b8dded25cd1eeb82772f88
suc1 <ytsucheng@gmail.com>
2017-06-07 09:17:29 -0400

    Test commot

--- Ch3SaveSvg/mainwindow.cpp
+++ Ch3SaveSvg/mainwindow.cpp
@@ -36,10 +36,8 @@ void MainWindow::paintEvent(QPaintEvent *event)
 void MainWindow::paintAll(QSvgGenerator *generator/* = 0*/)
 {
     QPainter painter;
-    if (generator)
-    painter.begin(generator);
-    else
-    painter.begin(this);
+    if (generator) painter.begin(generator);
+    else painter.begin(this);
 
     painter.setFont(QFont("Times", 14, QFont::Bold));
     painter.drawText(QPoint(20, 50), "Testing");
@@ -75,7 +73,7 @@ void MainWindow::on_actionSave_as_SVG_triggered()
     QString filePath = QFileDialog::getSaveFileName(this, "Save SVG", "", "SVG files (*.svg)");
     if (filePath == "") return;
 
-    //save SVG
+    //save SVG //Test git
     QSvgGenerator generator;
      generator.setFileName(filePath);
      generator.setSize(QSize(this->width(), this->height()));
